4d93f9f9e6035cf95cf696887e0ec95603ee19be
ijat <$y4f1q93>
2016-12-11 03:13:26 +0800

    First commit

---
+++ src/main/java/my/ijat/spsystem/controller.java
@@ -0,0 +1,16 @@
+package my.ijat.spsystem;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+public class controller {
+
+    @FXML
+    public Button btn;
+
+    public void press(ActionEvent event) {
+        System.out.println("dfg");
+    }
+}
